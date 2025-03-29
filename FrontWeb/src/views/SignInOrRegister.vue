d3036a54f14cba401fcd15bf4b8092c11dbbfefe
Matteo Faccetta <matteofaccetta@gmail.com>
2025-03-29 08:52:33 +0100

    finito con il refactor

--- FrontWeb/src/views/SignInOrRegister.vue
+++ FrontWeb/src/views/SignInOrRegister.vue
@@ -1,29 +1,18 @@
-<script>
+<script setup>
+import { ref } from 'vue';
 import LoginForm from '@/components/LoginForm.vue';
 import RegisterStudentForm from '../components/RegisterStudentForm.vue'
 import RegisterTeacherForm from '../components/RegisterTeacherForm.vue'
 
-export default {
-    components: {
-        RegisterStudentForm,
-        LoginForm,
-        RegisterTeacherForm
-    },
-    data() {
-        return {
-            action: 'registerStudent'
-        }
-    },
-    methods:{
-        changeState(whatToView){
-            this.action = whatToView
-        }
-    }
+const action = ref('registerStudent')
+function changeState(whatToView)
+{
+    action.value = whatToView
 }
 </script>
 
 <template>
-    <RegisterStudentForm v-if="this.action == 'registerStudent'" @change-state="changeState" />
-    <RegisterTeacherForm v-if="this.action == 'registerTeacher'" @change-state="changeState" />
-    <LoginForm v-if="this.action == 'log'" @change-state="changeState" />
+    <RegisterStudentForm v-if="action == 'registerStudent'" @change-state="changeState" />
+    <RegisterTeacherForm v-if="action == 'registerTeacher'" @change-state="changeState" />
+    <LoginForm v-if="action == 'log'" @change-state="changeState" />
 </template>
