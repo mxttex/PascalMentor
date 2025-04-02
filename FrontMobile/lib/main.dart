f18d7fb75923585e004f37ee8c35a5371d0baf1c
Nikorasukun <kopanterini@gmail.com>
2025-04-02 13:42:26 +0200

    aggiunto side insegnante mobile di registrazione

--- FrontMobile/lib/main.dart
+++ FrontMobile/lib/main.dart
@@ -17,9 +17,14 @@ class SimpleChat extends StatelessWidget {
     return MaterialApp(
       initialRoute: '/',
       routes: {
-        '/': (context) => LoginPage(title: 'NM Chat'),
-        '/mainstudente': (context) => MainStudente(),
-        '/formregistrazionestudente': (context) => FormRegistrazioneStudente(),
+        '/': (context) => const LoginPage(title: 'NM Chat'),
+        '/mainstudente': (context) => const MainStudente(),
+        '/maininsegnante': (context) => const MainInsegnante(),
+        '/formregistrazionestudente': (context) =>
+            const FormRegistrazioneStudente(),
+        '/formloginstudente': (context) => const FormLoginStudente(),
+        '/formregistrazioneinsegnante': (context) =>
+            const FormRegistrazioneInsegnante(),
       },
       title: 'Flutter Chat',
       theme: ThemeData(
@@ -77,7 +82,9 @@ class _LoginPageState extends State<LoginPage> {
             ),
             const Padding(padding: EdgeInsets.only(left: 20.0, right: 20.0)),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () {
+                Navigator.pushNamed(context, '/maininsegnante');
+              },
               style: const ButtonStyle(
                 backgroundColor: WidgetStatePropertyAll<Color>(
                     Color.fromARGB(255, 139, 241, 255)),
@@ -125,6 +132,50 @@ class _MainStudenteState extends State<MainStudente> {
               ElevatedButton(
                   onPressed: () {
                     Navigator.pushNamed(context, '/formregistrazionestudente');
+                  },
+                  child: const Text('Registrati')),
+              ElevatedButton(onPressed: () {}, child: const Text('Login')),
+            ],
+          ),
+        ));
+  }
+}
+
+//la pagina principale che viene selezionata quando si seleziona la modalità 'insegnante'
+class MainInsegnante extends StatefulWidget {
+  const MainInsegnante({super.key});
+
+  @override
+  State<MainInsegnante> createState() => _MainInsegnanteState();
+}
+
+class _MainInsegnanteState extends State<MainInsegnante> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(Icons.arrow_back)),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Insegnante',
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const Padding(padding: EdgeInsets.only(top: 100.0, bottom: 75.0)),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(
+                        context, '/formregistrazioneinsegnante');
                   },
                   child: const Text('Registrati')),
               ElevatedButton(onPressed: () {}, child: const Text('Login')),
@@ -202,8 +253,7 @@ class _FormRegistrazioneStudenteState extends State<FormRegistrazioneStudente> {
           context: context,
           type: CoolAlertType.error,
           title: "Errore",
-          text:
-              "ricontrolla i tuoi dati.");
+          text: "ricontrolla i tuoi dati.");
       return;
     }
     CoolAlert.show(
@@ -222,12 +272,19 @@ class _FormRegistrazioneStudenteState extends State<FormRegistrazioneStudente> {
     };
 
     try {
-      var responseHttp = await http.post(Uri.parse('http://192.168.189.83:8089/api/registerStudent'),headers: {'Content-type':'application/json'}, body: jsonEncode(message));
+      await http.post(
+          Uri.parse('http://192.168.189.83:8089/api/registerStudent'),
+          headers: {'Content-type': 'application/json'},
+          body: jsonEncode(message));
+
+      //Navigator.pushNamed(context, 'pagg');
     } catch (exception) {
-      CoolAlert.show(
-          context: context,
-          type: CoolAlertType.success,
-          text: exception.toString());
+      if (mounted) {
+        CoolAlert.show(
+            context: context,
+            type: CoolAlertType.success,
+            text: exception.toString());
+      }
     }
   }
 
@@ -376,6 +433,209 @@ class _FormLoginStudenteState extends State<FormLoginStudente> {
               icon: const Icon(Icons.arrow_back)),
           title: const Text("Login Studente"),
         ),
-        body: Center());
+        body: const Center());
+  }
+}
+
+class FormRegistrazioneInsegnante extends StatefulWidget {
+  const FormRegistrazioneInsegnante({super.key});
+
+  @override
+  State<FormRegistrazioneInsegnante> createState() =>
+      _FormRegistrazioneInsegnanteState();
+}
+
+class _FormRegistrazioneInsegnanteState
+    extends State<FormRegistrazioneInsegnante> {
+  TextEditingController firstNameText = TextEditingController();
+  TextEditingController lastNameText = TextEditingController();
+  TextEditingController emailText = TextEditingController();
+  TextEditingController pswText = TextEditingController();
+  TextEditingController confirmPasswordText = TextEditingController();
+  TextEditingController dataDiNascitaText = TextEditingController();
+  DateTime _selectedDate = DateTime.now();
+
+  @override
+  void dispose() {
+    firstNameText.dispose();
+    lastNameText.dispose();
+    emailText.dispose();
+    pswText.dispose();
+    confirmPasswordText.dispose();
+    dataDiNascitaText.dispose();
+
+    super.dispose();
+  }
+
+  Future<void> _pickDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: _selectedDate ?? DateTime.now(),
+      firstDate: DateTime(1900, 1),
+      lastDate: DateTime.now(),
+    );
+
+    if (picked != null && picked != _selectedDate) {
+      setState(() {
+        _selectedDate = picked;
+        dataDiNascitaText.text = DateFormat('yyyy-MM-dd').format(picked);
+      });
+    }
+  }
+
+  //method to encrypt in sha1 hash function
+  Digest sha1encrypt(element) {
+    var bytes = utf8.encode(element);
+
+    return sha1.convert(bytes);
+  }
+
+  void confermaInvioDati() async {
+    if (firstNameText.text == '' ||
+        lastNameText.text == '' ||
+        emailText.text == '' ||
+        pswText.text == '' ||
+        confirmPasswordText.text == '' ||
+        pswText.text != confirmPasswordText.text ||
+        dataDiNascitaText.text == '') {
+      CoolAlert.show(
+          context: context,
+          type: CoolAlertType.error,
+          title: "Errore",
+          text: "ricontrolla i tuoi dati.");
+      return;
+    }
+    CoolAlert.show(
+        context: context,
+        type: CoolAlertType.confirm,
+        title: "Conferma",
+        text: "la registrazione è avvenuta con successo.");
+    //nome,cognome,email,password,indirizzo,dataNascita
+    var message = {
+      'nome': firstNameText.text,
+      'cognome': lastNameText.text,
+      'email': emailText.text,
+      'password': sha1encrypt(pswText.text).toString(),
+      'dataNascita': dataDiNascitaText.text
+    };
+
+    try {
+      await http.post(
+          Uri.parse('http://192.168.189.83:8089/api/registerTeacher'),
+          headers: {'Content-type': 'application/json'},
+          body: jsonEncode(message));
+      
+      //Navigator.pushNamed(context, 'pagg');
+    } catch (exception) {
+      if (mounted) {
+        CoolAlert.show(
+            context: context,
+            type: CoolAlertType.success,
+            text: exception.toString());
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(Icons.arrow_back)),
+          title: const Text("Registrazione Insegnante"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //first name
+              SizedBox(
+                width: 200,
+                child: TextField(
+                  controller: firstNameText,
+                  decoration: const InputDecoration(
+                    labelText: 'Input your first name',
+                    hintText: 'Matteo',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              //last name
+              SizedBox(
+                width: 200,
+                child: TextField(
+                  controller: lastNameText,
+                  decoration: const InputDecoration(
+                    labelText: 'Input your last name',
+                    hintText: 'Faccetta',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              //email
+              SizedBox(
+                width: 200,
+                child: TextField(
+                  controller: emailText,
+                  decoration: const InputDecoration(
+                    labelText: 'Input your email',
+                    hintText: 'example@something.xxx',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              //password
+              SizedBox(
+                width: 200,
+                child: TextField(
+                  controller: pswText,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    labelText: 'Input your password',
+                    hintText: '12345',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              //confirming password
+              SizedBox(
+                width: 200,
+                child: TextField(
+                  controller: confirmPasswordText,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    labelText: 'Confirm you password',
+                    hintText: '*****',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              //dataDiNascita
+              SizedBox(
+                width: 200,
+                child: TextField(
+                  controller: dataDiNascitaText,
+                  readOnly: true,
+                  decoration: const InputDecoration(
+                    labelText: 'Date of Birth',
+                    hintText: 'Select your date of birth',
+                    border: OutlineInputBorder(),
+                  ),
+                  onTap: () {
+                    _pickDate(context);
+                  },
+                ),
+              ),
+              //bottone di conferma
+              ElevatedButton(
+                onPressed: confermaInvioDati,
+                child: const Text('Register'),
+              )
+            ],
+          ),
+        ));
   }
 }
