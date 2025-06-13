ada163cbc37578abc9f8f358fca7fd71311f4b20
Matteo Faccetta <113626542+mxttex@users.noreply.github.com>
2025-06-13 12:12:25 +0200

    sistemate foreign keys

--- scripts/CreazioneDB.sql
+++ scripts/CreazioneDB.sql
@@ -1,46 +1,56 @@
-CREATE DATABASE PascalMentor
-go
-USE PascalMentor
-go 
+CREATE DATABASE PascalMentor;
+GO
 
-CREATE TABLE Materie(
-	Id int identity(1,1) PRIMARY KEY,
+USE PascalMentor;
+GO 
+
+CREATE TABLE Materie (
+	Id INT IDENTITY(1,1) PRIMARY KEY,
 	Nome VARCHAR(40)
-)
-CREATE TABLE Utenti(
-	ID int identity(1,1) PRIMARY KEY,
+);
+
+CREATE TABLE Utenti (
+	ID INT IDENTITY(1,1) PRIMARY KEY,
 	Nome VARCHAR(20) NOT NULL,
 	Cognome VARCHAR(20) NOT NULL,
-	Tipo CHAR(1) DEFAULT('S'),
+	Tipo CHAR(1) DEFAULT 'S',
 	Mail VARCHAR(50) UNIQUE NOT NULL,
 	Password CHAR(64) NOT NULL,
-	RatingMedio FLOAT DEFAULT(0),
+	RatingMedio FLOAT DEFAULT 0,
 	DataDiNascita DATE NOT NULL,
 	DataIscrizione DATE NOT NULL,
 	IndirizzoDiStudio VARCHAR(20)
-	)
+);
 
-
-CREATE TABLE Ripetizioni(
-	Id int identity(1,1) PRIMARY KEY,
+CREATE TABLE Ripetizioni (
+	Id INT IDENTITY(1,1) PRIMARY KEY,
 	Data DATE NOT NULL,
 	OraInizio TIME NOT NULL,
 	OraFine TIME,
-	NumeroMassimoPartecipanti INT DEFAULT(1),
-	NumeroIscritti INT DEFAULT(0),
-	FOREIGN KEY Insegnante INT REFERENCES Utenti(Id) NOT NULL,
-	FOREIGN KEY Materia INT REFERENCES Materie(Id) NOT NULL,
-	Note VARCHAR(150))
+	NumeroMassimoPartecipanti INT DEFAULT 1,
+	NumeroIscritti INT DEFAULT 0,
+	Insegnante INT NOT NULL,
+	Materia INT NOT NULL,
+	Note VARCHAR(150),
+	FOREIGN KEY (Insegnante) REFERENCES Utenti(Id),
+	FOREIGN KEY (Materia) REFERENCES Materie(Id)
+);
 
-CREATE TABLE Partecipazioni(
-	FOREIGN KEY Studente INT REFERENCES Utenti(Id),
-	FOREIGN KEY Ripetizione INT REFERENCES Ripetizioni(Id)
-	FeedbackGiaLasciato TINYINT DEFAULT(0)
-	PRIMARY KEY(Studente, Ripetizione))
+CREATE TABLE Partecipazioni (
+	Studente INT NOT NULL,
+	Ripetizione INT NOT NULL,
+	FeedbackGiaLasciato TINYINT DEFAULT 0,
+	PRIMARY KEY (Studente, Ripetizione),
+	FOREIGN KEY (Studente) REFERENCES Utenti(Id),
+	FOREIGN KEY (Ripetizione) REFERENCES Ripetizioni(Id)
+);
 
-CREATE TABLE Feedbacks(
-	Id int identity(1,1) PRIMARY KEY,
+CREATE TABLE Feedbacks (
+	Id INT IDENTITY(1,1) PRIMARY KEY,
 	Rating FLOAT,
 	Descrizione VARCHAR(150),
-	FOREIGN KEY Studente INT REFERENCES Utenti(Id),
-	FOREIGN KEY Ripetizione INT REFERENCES Ripetizioni(Id))
+	Studente INT,
+	Ripetizione INT,
+	FOREIGN KEY (Studente) REFERENCES Utenti(Id),
+	FOREIGN KEY (Ripetizione) REFERENCES Ripetizioni(Id)
+);
